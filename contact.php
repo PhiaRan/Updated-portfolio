be0b7afa0d095d3de8f0682303aaa23a74f5aec7
Sophia R <sophie.g.rankin@gmail.com>
2022-07-22 20:26:30 -0400

    initial commit

---
+++ contact.php
@@ -0,0 +1,42 @@
+<?php
+  
+if($_POST) {
+    $name = "";
+    $email = "";
+    $subject = "";
+
+      
+    if(isset($_POST['name'])) {
+        $name = filter_var($_POST['name'], FILTER_SANITIZE_STRING);
+        $email_body .= "<div>
+                           <label><b>name:</b></label>&nbsp;<span>".$name."</span>
+                        </div>";
+    }
+ 
+    if(isset($_POST['email'])) {
+        $email = str_replace(array("\r", "\n", "%0a", "%0d"), '', $_POST['email']);
+        $email = filter_var($email, FILTER_VALIDATE_EMAIL);
+        $email_body .= "<div>
+                           <label><b>email:</b></label>&nbsp;<span>".$email."</span>
+                        </div>";
+    }
+      
+    if(isset($_POST['subject'])) {
+        $subject = filter_var($_POST['subject'], FILTER_SANITIZE_STRING);
+        $subject .= "<div>
+                           <label><b>Reason For Contacting Us:</b></label>&nbsp;<span>".$email."</span>
+                        </div>";
+
+
+
+                        if{ $recipient = "sophie.g.rankin@gmail.com";
+                    }
+                    
+
+    $subject .= "</div>";
+ 
+    $headers  = 'MIME-Version: 1.0' . "\r\n"
+    .'Content-type: text/html; charset=utf-8' . "\r\n"
+    .'From: ' . $email . "\r\n";
+
+?>
